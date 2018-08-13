3628237315a74ffbf324554c54f5775cb73a9e64
James Hunt <james@niftylogic.com>
2018-08-13 01:31:49 +0000

    Tweak compilation

--- compile.lisp
+++ compile.lisp
@@ -1,5 +1,16 @@
+#!/usr/bin/sbcl --script
+(require "sb-posix")
+(require "asdf")
+(asdf:load-system :daemon)
+
+(load "shout.lisp")
+
+(defun env (name default)
+  (or (sb-posix:getenv name)
+      default))
+
 (sb-ext:save-lisp-and-die
-  "app"
+  "shout"
   :compression t
   :executable  t
   :toplevel (lambda ()
@@ -11,5 +22,7 @@
               (format t "##    ## ##     ## ##     ## ##     ##    ##    #### ~%")
               (format t " ######  ##     ##  #######   #######     ##    #### ~%")
               (format t "starting up...~%")
-              (api:run :port 8199)
-              (loop (sleep 5))))
+              (daemon:daemonize :exit-parent t
+                                :pidfile (sb-posix:getenv "PID_FILE"))
+              (api:run :port    (env "SHOUT_PORT"     api::*default-port*)
+                       :dbfile  (env "SHOUT_DATABASE" api::*default-dbfile*))))
