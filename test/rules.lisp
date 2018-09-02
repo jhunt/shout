3decbe5a76b09f73646bb175153dee8fbb6fce29
James Hunt <james@niftylogic.com>
2018-09-02 08:03:27 -0400

    Lisp-ify the test suite

--- test/rules.lisp
+++ test/rules.lisp
@@ -10,6 +10,7 @@
   'made-it
   #'(lambda (args)
       (setf *args* args)))
+
 (defun triggers (what &optional msg)
   (is *args* (list what)
       (or msg
@@ -19,6 +20,12 @@
   (cond ((symbolp form) (symbol-name form))
         ((atom form) (format nil "~S" form))
         (t (format nil "(~{~A ~})" (mapcar #'parse form)))))
+
+(defun try (rules &key params metadata)
+  (rules:eval/rules
+    (rules:load/rules (parse rules))
+    (or params *no-params*)
+    (or metadata *no-metadata*)))
 
 (plan nil)
 (subtest "Plugin Registration"
@@ -30,235 +37,151 @@
       "'not-a-thing shoult not be registered"))
 
 (subtest "Evaluation"
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                  (when *
-                    (made-it "here"))))))
-    *no-params*
-    *no-metadata*)
+  (try `((for *
+           (when *
+             (made-it "here")))))
   (triggers "here" "FOR * / WHEN * always fires")
 
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                  (when *
-                    (made-it "no further than here"))
-                  (when (on weekdays)
-                    (made-it "to the weekday!"))
-                  (when (on weekends)
-                    (made-it "to the weekday!"))))))
-    *no-params*
-    *no-metadata*)
+  (try `((for *
+           (when *
+             (made-it "no further than here"))
+           (when (on weekdays)
+             (made-it "to the weekday!"))
+           (when (on weekends)
+             (made-it "to the weekday!")))))
   (triggers "no further than here"
       "Only the first matching WHEN should fire")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for "topic"
-                  (when * (made-it "to the topic")))
-               (for "off-topic"
-                  (when * (made-it "wrong"))))))
-    (pairlis '(:topic) '("topic"))
-    *no-metadata*)
+  (try `((for "topic"
+           (when * (made-it "to the topic")))
+             (for "off-topic"
+           (when * (made-it "wrong"))))
+       :params (pairlis '(:topic) '("topic")))
   (triggers "to the topic"
       "FOR should match topic expicitly")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for (or (is "topic")
-                        (is "something else"))
-                  (when * (made-it "to the topic")))
-               (for "off-topic"
-                  (when * (made-it "wrong"))))))
-    (pairlis '(:topic) '("topic"))
-    *no-metadata*)
+  (try `((for (or (is "topic")
+                  (is "something else"))
+           (when * (made-it "to the topic")))
+         (for "off-topic"
+           (when * (made-it "wrong"))))
+       :params (pairlis '(:topic) '("topic")))
   (triggers "to the topic"
       "FOR should match topic in an expression tree")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                 (when ((metadata? super-important))
-                   (made-it "shouldn't make it..."))
-                 (when ((metadata? escalate))
-                   (made-it (metadata escalate)))
-                 (when * (made-it "normal"))))))
-    *no-params*
-    (pairlis '(:escalate) '("escalated")))
+  (try `((for *
+           (when ((metadata? super-important))
+             (made-it "shouldn't make it..."))
+           (when ((metadata? escalate))
+             (made-it (metadata escalate)))
+           (when * (made-it "normal"))))
+       :metadata (pairlis '(:escalate) '("escalated")))
   (triggers "escalated" "Metadata evaluates properly")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((set msg "a message")
-               (for *
-                  (when *
-                    (made-it (value msg)))))))
-    *no-params*
-    *no-metadata*)
+  (try `((set msg "a message")
+         (for *
+           (when *
+             (made-it (value msg))))))
   (triggers "a message" "Variables should work")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((set hooks (map "default"  "the default"
-                               "override" "something"))
-               (for *
-                  (when *
-                    (made-it (lookup hooks "override" "default")))))))
-    *no-params*
-    *no-metadata*)
+  (try `((set hooks (map "default"  "the default"
+                         "override" "something"))
+         (for *
+           (when *
+             (made-it (lookup hooks "override" "default"))))))
   (triggers "something" "Map lookups with variable keys should work")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((set hooks (map "this" "to that"))
-               (for *
-                  (when ((lookup hooks "x" "y" "z"))
-                    (made-it "to the wrong place"))
-                  (when *
-                    (made-it (lookup hooks "this")))))))
-    *no-params*
-    *no-metadata*)
+  (try `((set hooks (map "this" "to that"))
+         (for *
+           (when ((lookup hooks "x" "y" "z"))
+             (made-it "to the wrong place"))
+           (when *
+             (made-it (lookup hooks "this"))))))
   (triggers "to that" "Map lookups work in conditionals and value positions")
 
   (let ((ruleset
-          (rules:load/rules
-            (parse
-              `((for *
-                  (when ((or a b))
-                    (made-it "a or b was true"))
-                  (when *
-                    (made-it "neither were true"))))))))
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:a :b) '(t t))
-      *no-metadata*)
+          `((for *
+              (when ((or a b))
+                (made-it "a or b was true"))
+              (when *
+                (made-it "neither were true"))))))
+    (try ruleset :params (pairlis '(:a :b) '(t t)))
     (triggers "a or b was true" "(OR t t) is true")
 
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:a :b) '(t nil))
-      *no-metadata*)
+    (try ruleset :params (pairlis '(:a :b) '(t nil)))
     (triggers "a or b was true" "(OR t nil) is true")
 
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:a :b) '(nil t))
-      *no-metadata*)
+    (try ruleset :params (pairlis '(:a :b) '(nil t)))
     (triggers "a or b was true" "(OR nil t) is true")
 
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:a :b) '(nil nil))
-      *no-metadata*)
+    (try ruleset :params (pairlis '(:a :b) '(nil nil)))
     (triggers "neither were true" "(OR nil nil) is false"))
 
   (let ((ruleset
-          (rules:load/rules
-            (parse
-              `((for *
-                  (when ((not a))
-                    (made-it "(NOT a) was true"))
-                  (when *
-                    (made-it "(NOT a) was false"))))))))
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:a) '(t))
-      *no-metadata*)
+          `((for *
+              (when ((not a))
+                (made-it "(NOT a) was true"))
+              (when *
+                (made-it "(NOT a) was false"))))))
+    (try ruleset :params (pairlis '(:a) '(t)))
     (triggers "(NOT a) was false")
 
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:a) '(nil))
-      *no-metadata*)
+    (try ruleset :params (pairlis '(:a) '(nil)))
     (triggers "(NOT a) was true"))
 
   (let ((ruleset
-          (rules:load/rules
-            (parse
-              `((for *
-                  (when *
-                    (made-it (if test "consequence" "alternate")))))))))
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:test) '(t))
-      *no-metadata*)
+          `((for *
+              (when *
+                (made-it (if test "consequence" "alternate")))))))
+    (try ruleset :params (pairlis '(:test) '(t)))
     (triggers "consequence"
               "(IF t ...) should trigger the consequent")
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:test) '(nil))
-      *no-metadata*)
+    (try ruleset :params (pairlis '(:test) '(nil)))
     (triggers "alternate"
               "(IF nil ...) should trigger the alternate"))
 
   (let ((ruleset
-          (rules:load/rules
-            (parse
-              `((for *
-                  (when *
-                    (made-it (if test "consequence")))))))))
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:test) '(t))
-      *no-metadata*)
+          `((for *
+              (when *
+                (made-it (if test "consequence")))))))
+    (try ruleset :params (pairlis '(:test) '(t)))
     (triggers "consequence"
               "(IF t ...) should trigger the consequent")
-    (rules:eval/rules
-      ruleset
-      (pairlis '(:test) '(nil))
-      *no-metadata*)
+    (try ruleset :params (pairlis '(:test) '(nil)))
     (triggers nil "A nil alternate for (IF ...) is ok"))
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((set var "variable")
-               (for *
-                  (when *
-                    (made-it (concat "this is a" (value var))))))))
-    *no-params*
-    *no-metadata*)
+  (try `((set var "variable")
+         (for *
+           (when *
+             (made-it (concat "this is a" (value var)))))))
   (triggers
     (format nil "this is a~%variable~%")
     "Concat combines strings mapped with trailing newlines")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                  (when *
-                    (made-it message))))))
-    (pairlis '(:message) '("a message"))
-    *no-metadata*)
+  (try `((for *
+           (when *
+             (made-it message))))
+       :params (pairlis '(:message) '("a message")))
   (triggers "a message"
     "Symbols evaluate to the named parameter")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                  (when *
-                    (made-it nil))))))
-    *no-params*
-    *no-metadata*)
+  (try `((for *
+           (when *
+             (made-it nil)))))
   (triggers nil "NIL evaluates to nil")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                  (when *
-                    (made-it "this is a $message"))))))
-    (pairlis '(:message) '("parameter"))
-    *no-metadata*)
+  (try `((for *
+           (when *
+             (made-it "this is a $message"))))
+       :params (pairlis '(:message) '("parameter")))
   (triggers "this is a parameter"
     "String interpolation of parameters works")
 
-  (rules:eval/rules
-    (rules:load/rules
-      (parse `((for *
-                  (when *
-                    (made-it "this is $[meta]"))))))
-    *no-params*
-    (pairlis '(:meta) '("metadata")))
+  (try `((for *
+           (when *
+             (made-it "this is $[meta]"))))
+       :metadata (pairlis '(:meta) '("metadata")))
   (triggers "this is metadata"
     "String interpolation of metadata works"))
 
@@ -268,113 +191,60 @@
     ; SkyNet has become self-aware.
     ; How do we notify about that?
 
-    (rules:eval/rules
-      (rules:load/rules
-        (parse `((for *
-                   (when ((on weekends))
-                     (made-it "not yet..."))
-                   (when ((on weekdays))
-                     (made-it "skynet has become self-aware"))
-                   (when * (made-it "wrong"))))))
-      *no-params*
-      *no-metadata*)
+    (try `((for *
+             (when ((on weekends))
+               (made-it "not yet..."))
+             (when ((on weekdays))
+               (made-it "skynet has become self-aware"))
+             (when * (made-it "wrong")))))
     (triggers "skynet has become self-aware")
 
-    (rules:eval/rules
-      (rules:load/rules
-        (parse `((for *
-                   (when ((on friday))
-                     (made-it "skynet has become self-aware"))
-                   (when * (made-it "wrong"))))))
-      *no-params*
-      *no-metadata*)
+    (try `((for *
+             (when ((on friday))
+               (made-it "skynet has become self-aware"))
+             (when * (made-it "wrong")))))
     (triggers "skynet has become self-aware")
 
-    (rules:eval/rules
-      (rules:load/rules
-        (parse `((for *
-                   (when ((after 0800 am))
-                     (made-it "too late"))
-                   (when * (made-it "correct"))))))
-      *no-params*
-      *no-metadata*)
+    (try `((for *
+             (when ((after 0800 am))
+               (made-it "too late"))
+             (when * (made-it "correct")))))
     (triggers "correct")
 
-    (rules:eval/rules
-      (rules:load/rules
-        (parse `((for *
-                   (when ((before 0800 am))
-                     (made-it "on time"))
-                   (when * (made-it "wrong"))))))
-      *no-params*
-      *no-metadata*)
+    (try `((for *
+             (when ((before 0800 am))
+               (made-it "on time"))
+             (when * (made-it "wrong")))))
     (triggers "on time")
 
-    (rules:eval/rules
-      (rules:load/rules
-        (parse `((for *
-                   (when ((after 0100 pm))
-                     (made-it "too late"))
-                   (when * (made-it "on time"))))))
-      *no-params*
-      *no-metadata*)
+    (try `((for *
+             (when ((after 0100 pm))
+               (made-it "too late"))
+             (when * (made-it "on time")))))
     (triggers "on time")
 
-    (rules:eval/rules
-      (rules:load/rules
-        (parse `((for *
-                   (when ((from 0100 am to 0300 am))
-                     (made-it "on time"))
-                   (when * (made-it "wrong"))))))
-      *no-params*
-      *no-metadata*)
+    (try `((for *
+             (when ((from 0100 am to 0300 am))
+               (made-it "on time"))
+             (when * (made-it "wrong")))))
     (triggers "on time")
-
 
     (ok t))
   (ok t))
 
 (subtest "Syntax errors"
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((set 42))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((set var))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((foo bar))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((for * (foo bar)))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((for * (when * bar)))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((for * (when * (foo bar))))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((for * (when ((foo bar)) (made-it "fail"))))))
-              *no-params*
-              *no-metadata*) 'simple-error)
-  (is-error (rules:eval/rules
-              (rules:load/rules
-                (parse `((for * (when ((after 0800 bc)) (made-it "fail"))))))
-              *no-params*
-              *no-metadata*) 'simple-error)
+  (defun is-syntax-error (form &key params metadata)
+    (is-error (try form :params params :metadata metadata)
+              'simple-error))
+
+  (is-syntax-error `((set 42)))
+  (is-syntax-error `((set var)))
+  (is-syntax-error `((foo bar)))
+  (is-syntax-error `((for * (foo bar))))
+  (is-syntax-error `((for * (when * bar))))
+  (is-syntax-error `((for * (when * (foo bar)))))
+  (is-syntax-error `((for * (when ((foo bar)) (made-it "fail")))))
+  (is-syntax-error `((for * (when ((after 0800 bc)) (made-it "fail")))))
   (ok t))
 
 (finalize)
